063a0b3d570547b9466ca6461210732a37c47860
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-21 11:08:56 +0530

    changes ui

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import AddNewInterview from './(components)/AddNewInterview'
-import InterviewList from './(components)/InterviewList'
+import React from 'react';
+import AddNewInterview from './(components)/AddNewInterview';
+import InterviewList from './(components)/InterviewList';
 
 const Dashboard = () => {
   return (
-    <div className='p-10'>
-      <h2 className='font-bold text-2xl'>Dashboard</h2>
-      <h2 className='text-gray-500'>Create and start your AI mock Interview</h2>
-      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
-        <AddNewInterview/>
+    <div className="p-10 bg-white dark:bg-gray-900 text-black dark:text-white">
+      <h2 className="font-bold text-2xl">Dashboard</h2>
+      <h2 className="text-gray-500 dark:text-gray-400">Create and start your AI mock Interview</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 my-5 gap-6">
+        <AddNewInterview />
       </div>
-        <InterviewList/>
+      <InterviewList />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
