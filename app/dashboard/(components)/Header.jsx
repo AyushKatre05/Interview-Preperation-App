39d729ce4cf41fd0b5d2eb698f061701a591cc81
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-21 11:09:34 +0530

    added theme tiggle button

--- app/dashboard/(components)/Header.jsx
+++ app/dashboard/(components)/Header.jsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 import { useState } from 'react';
 import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import ThemeToggleButton from './ThemeToggleButton';
 
 const Header = () => {
   const [path, setPath] = useState('/dashboard');
@@ -18,10 +19,10 @@ const Header = () => {
   };
 
   return (
-    <header className="flex flex-col md:flex-row items-center justify-between p-4 bg-white shadow-md">
+    <header className="flex flex-col md:flex-row items-center justify-between p-4 bg-white dark:bg-gray-800 shadow-md">
       <div className="flex items-center justify-between w-full md:w-auto">
         <div className="flex items-center">
-          <Link href={'/'}><Image src="/logo.svg" alt="Logo" height={100} width={160} className="h-12 w-auto"/></Link>
+          <Link href={'/'}><Image src="/logo.svg" alt="Logo" height={100} width={160} className="h-12 w-auto" /></Link>
         </div>
         <button
           className="md:hidden focus:outline-none"
@@ -29,7 +30,7 @@ const Header = () => {
           aria-label={isOpen ? 'Close menu' : 'Open menu'}
         >
           <svg
-            className="h-6 w-6 text-gray-600"
+            className="h-6 w-6 text-gray-600 dark:text-white"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -56,30 +57,36 @@ const Header = () => {
       <nav className={`w-full md:w-auto mt-4 md:mt-0 md:flex md:items-center ${isOpen ? 'block' : 'hidden'}`}>
         <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 text-lg font-medium">
           <li
-            className={`cursor-pointer ${path === '/dashboard' ? 'font-bold border-b-2 border-primary text-primary' : 'text-gray-700 hover:text-primary'}`}
+            className={`cursor-pointer ${path === '/dashboard' ? 'font-bold border-b-2 border-primary text-primary dark:border-primary dark:text-primary' : 'text-gray-700 hover:text-primary dark:text-gray-300 dark:hover:text-primary'}`}
             onClick={() => navigateTo('/dashboard')}
           >
             <Link href={'/dashboard'}>Dashboard</Link>
           </li>
           <li
-            className={`cursor-pointer ${path === '/dashboard/questions' ? 'font-bold border-b-2 border-primary text-primary' : 'text-gray-700 hover:text-primary'}`}
+            className={`cursor-pointer ${path === '/dashboard/questions' ? 'font-bold border-b-2 border-primary text-primary dark:border-primary dark:text-primary' : 'text-gray-700 hover:text-primary dark:text-gray-300 dark:hover:text-primary'}`}
             onClick={() => navigateTo('/dashboard/questions')}
           >
             <Link href={'/dashboard/questions'}>Questions</Link>
           </li>
           <li
-            className={`cursor-pointer ${path === '/dashboard/how-it-works' ? 'font-bold border-b-2 border-primary text-primary' : 'text-gray-700 hover:text-primary'}`}
+            className={`cursor-pointer ${path === '/dashboard/how-it-works' ? 'font-bold border-b-2 border-primary text-primary dark:border-primary dark:text-primary' : 'text-gray-700 hover:text-primary dark:text-gray-300 dark:hover:text-primary'}`}
             onClick={() => navigateTo('/dashboard/how-it-works')}
           >
             <Link href={'/dashboard/how-it-works'}>How It Works?</Link>
           </li>
           <li className="mt-4 md:mt-0 flex items-center md:hidden">
-            <UserButton />
+          <span className='flex flex-col gap-5 justify-center items-center'>
+          <UserButton />
+          <ThemeToggleButton />
+        </span>
           </li>
         </ul>
       </nav>
       <div className="hidden md:flex items-center">
-        <UserButton />
+        <span className='flex gap-5 justify-center items-center'>
+          <ThemeToggleButton />
+          <UserButton />
+        </span>
       </div>
     </header>
   );
